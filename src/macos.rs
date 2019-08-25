1bce8822cdc080589cdc1efe62cfc2d1e9c8b27b
Joseph Birr-Pixton <jpixton@gmail.com>
2019-08-25 17:42:30 +0100

    Deal with search error

--- src/macos.rs
+++ src/macos.rs
@@ -8,9 +8,10 @@ pub fn load_native_certs() -> Result<RootCertStore, Error> {
     let results = item::ItemSearchOptions::new()
         .class(item::ItemClass::certificate())
         .load_refs(true)
-        .search();
+        .search()
+        .map_err(|err| Error::new(ErrorKind::Other, err))?;
 
-    for result in results {
+    for result in &results {
         if let item::SearchResult::Ref(cert_ref) = result {
             if let item::Reference::Certificate(cert) = cert_ref {
                 store.add(&rustls::Certificate(cert.to_der()))
