09919ae0754fc2573cb9ff1572b72f66a8028746
Joseph Birr-Pixton <jpixton@gmail.com>
2019-10-16 20:35:49 +0100

    Clean up how platforms are imported
    This way works with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,19 @@
 //! It consists of a single function [load_native_certs](fn.load_native_certs.html) which returns a
 //! `rustls::RootCertStore` pre-filled from the native certificate store.
 
-#[cfg_attr(target_os = "linux", path = "linux.rs")]
-#[cfg_attr(target_family = "windows", path = "windows.rs")]
-#[cfg_attr(target_os = "macos", path = "macos.rs")]
-mod platform;
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+use linux as platform;
+
+#[cfg(target_family = "windows")]
+mod windows;
+#[cfg(target_family = "windows")]
+use windows as platform;
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+use macos as platform;
 
 pub use platform::load_native_certs;
