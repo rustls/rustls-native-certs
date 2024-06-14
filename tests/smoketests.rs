7bc9064e09661b56ef775764023671b7fcc38b78
Peter Gerber <peter@arbitrary.ch>
2024-06-14 12:48:07 +0000

    Tests: distinguish between TLS error and other errors
    Avoid catching panic without truly knowing what kind of error
    caused it. There could have been an network error or an error
    loading certificates triggering a panic. (Unlikely with the
    current test but there is more tests for failure conditions
    coming.)

--- tests/smoketests.rs
+++ tests/smoketests.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::path::PathBuf;
 use std::sync::Arc;
@@ -8,7 +8,16 @@ use std::{env, panic};
 // the global env var configuration in the env var test interferes with the others.
 use serial_test::serial;
 
-fn check_site(domain: &str) {
+/// Check if connection to site works
+///
+/// Yields an Err if and only if there is an issue connecting that
+/// appears to be due to a certificate problem.
+///
+/// # Panics
+///
+/// Panics on errors unrelated to the TLS connection like errors during
+/// certificate loading, or connecting via TCP.
+fn check_site(domain: &str) -> Result<(), ()> {
     let mut roots = rustls::RootCertStore::empty();
     for cert in rustls_native_certs::load_native_certs().unwrap() {
         roots.add(cert).unwrap();
@@ -27,7 +36,7 @@ fn check_site(domain: &str) {
     .unwrap();
     let mut sock = TcpStream::connect(format!("{}:443", domain)).unwrap();
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
-    tls.write_all(
+    let result = tls.write_all(
         format!(
             "GET / HTTP/1.1\r\n\
                        Host: {}\r\n\
@@ -37,55 +46,59 @@ fn check_site(domain: &str) {
             domain
         )
         .as_bytes(),
-    )
-    .unwrap();
+    );
+    match result {
+        Ok(()) => (),
+        Err(e) if e.kind() == ErrorKind::InvalidData => return Err(()), // TLS error
+        Err(e) => panic!("{}", e),
+    }
     let mut plaintext = [0u8; 1024];
     let len = tls.read(&mut plaintext).unwrap();
     assert!(plaintext[..len].starts_with(b"HTTP/1.1 ")); // or whatever
+    Ok(())
 }
 
 #[test]
 #[serial]
 fn google() {
-    check_site("google.com");
+    check_site("google.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn amazon() {
-    check_site("amazon.com");
+    check_site("amazon.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn facebook() {
-    check_site("facebook.com");
+    check_site("facebook.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn netflix() {
-    check_site("netflix.com");
+    check_site("netflix.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn ebay() {
-    check_site("ebay.com");
+    check_site("ebay.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn apple() {
-    check_site("apple.com");
+    check_site("apple.com").unwrap();
 }
 
 #[test]
 #[serial]
 fn badssl_with_env() {
-    let result = panic::catch_unwind(|| check_site("self-signed.badssl.com"));
     // Self-signed certs should never be trusted by default:
-    assert!(result.is_err());
+    assert!(check_site("self-signed.badssl.com").is_err());
 
     // But they should be trusted if SSL_CERT_FILE is set:
     env::set_var(
@@ -93,6 +106,6 @@ fn badssl_with_env() {
         // The CA cert, downloaded directly from the site itself:
         PathBuf::from("./tests/badssl-com-chain.pem"),
     );
-    check_site("self-signed.badssl.com");
+    check_site("self-signed.badssl.com").unwrap();
     env::remove_var("SSL_CERT_FILE");
 }
